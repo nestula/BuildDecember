7282baafbec274ecaa95ba6e700ea6b55668a053
XOTlC <twilightgeometrydash@gmail.com>
2025-03-21 14:13:21 +0000

    fix

--- server.js
+++ server.js
@@ -9,7 +9,39 @@ const favicon = require("serve-favicon");
 
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Use a different port if 3000 is in use -> 3333
+const PORT = process.env.PORT || 9999; // Use a different port if 3000 is in use -> 3333
+
+app.set('trust proxy', true);
+
+app.use((req, res, next) => {
+  // get ./private/resources/WebStats.json
+  const webStatsPath = path.join(__dirname, "private/resources/WebStats.json"); // Correct path
+
+  fs.readFile(webStatsPath, "utf8", (err, data) => {
+    if (err) {
+      console.log("Error reading file:", err);
+    } else {
+      const webStats = JSON.parse(data);
+      // visits
+      webStats.visits += 1;
+      // unique
+      if (!webStats.logged.includes(req.ip)) {
+        if (req.ip !== null) {
+          webStats.uniqueVisits += 1;
+          webStats.logged.push(req.ip);
+        }
+      }
+
+      fs.writeFile(webStatsPath, JSON.stringify(webStats, null, 2), "utf8", (err) => {
+        if (err) {
+          console.log("Error writing file:", err);
+        }
+      })
+    }
+  });
+
+  next()
+});
 
 // Serve static files from 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -42,33 +74,7 @@ app.listen(PORT, () => {
 
 ///// middleware /////
 
-app.use((req, res, next) => {
-  // get ./private/resources/WebStats.json
-  const webStatsPath = path.join(__dirname, "private/resources/WebStats.json"); // Correct path
 
-  fs.readFile(webStatsPath, "utf8", (err, data) => {
-    if (err) {
-      console.log("Error reading file:", err);
-    } else {
-      const webStats = JSON.parse(data);
-      // visits
-      webStats.visits += 1;
-      // unique
-      if(!webStats.logged.includes(req.ip)) {
-        webStats.uniqueVisits += 1;
-        webStats.logged.push(req.ip);
-      }
-      
-      fs.writeFile(webStatsPath, JSON.stringify(webStats, null, 2), "utf8", (err) => {
-        if (err) {
-          console.log("Error writing file:", err);
-        }
-      })
-    }
-  });
-
-  next()
-});
 
 app.get("/updateMetrics", (req, res) => {
   // console.log("Visit")
