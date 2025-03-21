ff75da2464f40acb603f8362f1b7a339c1763f54
XOTlC <twilightgeometrydash@gmail.com>
2025-03-21 12:42:42 +0000

    update fetch path for Rune JSON data

--- public/resources/modules/search.js
+++ public/resources/modules/search.js
@@ -7,7 +7,7 @@ async function getRuneJSONData() {
             return null; // Return null if data is already loaded
         }
         // Fetch the JSON data from the server
-        const response = await fetch("/getJSON");
+        const response = await fetch("/resources/modules/RuneList.json");
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
