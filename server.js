b9ef43d68c10d517b897f7fa5dd8ef33ef2fafc8
eakto.mp4 <evanbison25@gmail.com>
2025-03-27 21:06:07 -0400

    middleware

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
       try { webStats = JSON.parse(data); }
       catch { return; }
       // visits
-      webStats.visits += 1;
+      webStats.calls += 1;
       // unique
       if (!webStats.logged.includes(req.ip)) {
         if (req.ip !== null) {
